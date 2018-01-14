ba8d0d8fc2b117eb24b2147dc28cc91410843b0b
zenbatista <harald.batista@yahoo.com>
2018-01-13 21:07:57 -0800

    Works all the way through 59:31. Whehew!

--- Chapter11/EmojiArtViewController.swift
+++ Chapter11/EmojiArtViewController.swift
@@ -161,6 +161,20 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
                 item.dragItem,
                 to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlacehoderCell")
                 )
+                item.dragItem.itemProvider.loadObject(ofClass: NSAttributedString.self) { (provider, error) in
+                    DispatchQueue.main.async {
+                        if let attributedString = provider as? NSAttributedString {
+                            placeholderContext.commitInsertion(dataSourceUpdates: { insertionIndexPath in
+                                self.emojis.insert(attributedString.string, at: insertionIndexPath.item)
+                            })
+                                
+                         
+                        } else {
+                            placeholderContext.deletePlaceholder()
+                        }
+                    }
+                    
+                }
             }
         }
     }
